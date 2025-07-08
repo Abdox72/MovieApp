9bd5e9bbbd8a486b21dee71508040285de5945ce
Abdul-Rahman Mustafa <102931530+Abdox72@users.noreply.github.com>
2025-07-08 20:44:25 +0300

    Update environmet.ts

--- src/environmets/environmet.ts
+++ src/environmets/environmet.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiKey: '943a1d14054f1dcc52c2bc72de292ab7',
+  apiKey: '.............................',
   baseUrl: 'https://api.themoviedb.org/3'
 };
