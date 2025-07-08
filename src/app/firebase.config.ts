79c481bb46d2b1a164baf285e28c02f2bd820b40
Abdul-Rahman Mustafa <102931530+Abdox72@users.noreply.github.com>
2025-07-08 20:45:38 +0300

    Update firebase.config.ts

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,10 +1,10 @@
 import { initializeApp } from "firebase/app";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCsAULS4YQsR8y0GAb2MMQQEOU82JnOIHY",
+  apiKey: "..................................",
   authDomain: "movieapp-6d29d.firebaseapp.com",
   projectId: "movieapp-6d29d",
   storageBucket: "movieapp-6d29d.firebasestorage.app",
-  messagingSenderId: "1004782521562",
-  appId: "1:1004782521562:web:9921184c2b9c974f6869e3"
+  messagingSenderId: "..............",
+  appId: "....................................."
 };
